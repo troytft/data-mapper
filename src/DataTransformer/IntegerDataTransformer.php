ab760aaba724e00ed7c72dc0ea6ddacae2f4aefe
Ilya Pleshakov <da-eto@ya.ru>
2016-04-28 16:39:01 +0300

    Allow treat empty strings as null values in IntegerDataTransformer

--- src/DataTransformer/IntegerDataTransformer.php
+++ src/DataTransformer/IntegerDataTransformer.php
@@ -6,9 +6,17 @@ use Common\Exception\FormValidationFieldException;
 
 class IntegerDataTransformer extends BaseDataTransformer implements DataTransformerInterface
 {
+    private $emptyStringIsNull;
+
+    public function setOptions($options)
+    {
+        $this->emptyStringIsNull = !empty($options['emptyStringIsNull']);
+        parent::setOptions($options);
+    }
+
     public function transform($value)
     {
-        if ($value === null) {
+        if ($value === null || ($this->emptyStringIsNull && $value === '')) {
             return null;
         }
 
