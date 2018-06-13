0dabbea9220e8c8c691ee413f976c9c1df219824
Arseny Razin <spufru@gmail.com>
2018-06-13 16:44:49 +0300

    non-bool error

--- src/DataTransformer/BooleanDataTransformer.php
+++ src/DataTransformer/BooleanDataTransformer.php
@@ -2,6 +2,8 @@
 
 namespace Troytft\DataMapperBundle\DataTransformer;
 
+use Troytft\DataMapperBundle\Exception\ValidationFieldException;
+
 class BooleanDataTransformer extends BaseDataTransformer implements DataTransformerInterface
 {
     public function transform($value)
@@ -10,6 +12,12 @@ class BooleanDataTransformer extends BaseDataTransformer implements DataTransfor
             return null;
         }
 
-        return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+        $val = filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+        
+        if ($val === null) {
+            throw new ValidationFieldException($this->getPropertyName(), 'Значение должно быть булевым');
+        }
+        
+        return $val;
     }
 }
